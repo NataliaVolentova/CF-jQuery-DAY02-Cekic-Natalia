7d1c32f2caeecd6339535f24aaf69fb79ae33e67
Natalia <nvolentova@gmail.com>
2020-10-06 15:16:27 +0200

    intermediate

--- intermediate/alejandro/javascript.js
+++ intermediate/alejandro/javascript.js
@@ -5,16 +5,37 @@ var jsonData = JSON.parse(images);
 //that we previously parsed.
 
 //after creating the divs with the images, we select the id of the created image, and append the text as a span to it
-for (var i = 0; i < jsonData.length; i++) {
+
+for (let i = 0; i < jsonData.length; i++) {  //let makes i variable accessible for all functions, best practice
     $('.container').append(
         `<div id="${i}" class="img-container"><img id="img${i}"src='${jsonData[i].image}'></div>`,
     );
     $(`#${i}`).append(`<br><span>Relation: ${jsonData[i].relation}</span>`);
-    $(`#${i}`).append(`<br><span>Name: ${jsonData[i].name}</span>`);
+    $(`#${i}`).append(`<br><span>Name: ${jsonData[i].name}</span>`);    
+    
+    $(`#${i}`).on("click", function() {
+        $(this).append(`<br><span>Hobby: ${jsonData[i].hobby}<br></span>`);
+        console.log(jsonData[i].hobby);
+        
+    });
+
 }
+
+
+
+
+//remove family member
 $(document).ready(function() {
-    $(".img-container").on("click", function() {
-        var displayhobby =
+    $(".img-container").on("dblclick", function() {
+        $(this).hide();
 
     });
 });
+
+
+//add additional info-hobby
+
+    for (var i = 0; i < jsonData.length; i++){ 
+       
+    };
+
